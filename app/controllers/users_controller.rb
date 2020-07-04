23d8ed17c4c21f31848f103410fa951c02cf7d32
Stephen McBride <stevemcbride3@gmail.com>
2020-07-04 18:07:01 -0400

    Updated .gitigore, updated UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,27 @@
 class UsersController < ApplicationController
+    def new
+    end
+
+    def create
+        @user = User.new(user_params)
+        return redirect_to new_user_path if !@user.valid?
+        @user.save
+        redirect_to new_restaurant_path
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :name, :password)
+    end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+    def current_user
+        User.find(session[:user][:id])
+    end
+    def logged_in
+    end
 end
