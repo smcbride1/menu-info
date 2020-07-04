66568bdb06abde075b7f50663e28cc6b5298d1b9
Stephen McBride <stevemcbride3@gmail.com>
2020-07-04 17:25:59 -0400

    Updated routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,6 @@ Rails.application.routes.draw do
   resources :menu_items
   resources :menus
   resources :restaurants
-  resources :users
+  resources :users, except: [:index]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
