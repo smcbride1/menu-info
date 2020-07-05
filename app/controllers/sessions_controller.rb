1a1096fdb7ab6a3d1d59ed01ca08e085ad21b983
Stephen McBride <stevemcbride3@gmail.com>
2020-07-05 19:21:30 -0400

    Updated SessionsController, created new session view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    def new #login form
+    end
+
+    def create #login action
+        @user = User.find(user_params[:id])
+        if @user.authenticate(user_params[:password])
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            flash[:warning] = "Invalid email or password. Please try again."
+            redirect_to new_session_path
+        end
+    end
+
+    def destroy #logout
+        session.destroy
+    end
+
+    def user_params
+        params.require(:user).permit(:email, :password)
+    end
 end
