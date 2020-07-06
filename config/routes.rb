9840545645094446361c3b16de95d078f86872af
Stephen McBride <stevemcbride3@gmail.com>
2020-07-06 07:43:51 -0400

    Created root route, action, and view, fixed new view

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   post "logout", to: "sessions#destroy"
+  root "application#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
