61bdce246905b44ed15d6f16a0abb9d19f189c38
Stephen McBride <stevemcbride3@gmail.com>
2020-07-06 07:19:05 -0400

    Updated RestaurantsController, created new restaurant view

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,2 +1,20 @@
 class RestaurantsController < ApplicationController
+    def new
+        @restaurant = Restaurant.new
+    end
+
+    def create
+        @restaurant = Restaurant.new(restaurant_params)
+        @restaurant.user = current_user
+        if @restaurant.valid?
+            @restaurant.save
+            redirect_to restaurant_path(@restaurant)
+        else
+            render :new
+        end
+    end
+
+    def restaurant_params
+        params.require(:restaurant).permit(:name, :description, :website, :phone_number, :address)
+    end
 end
