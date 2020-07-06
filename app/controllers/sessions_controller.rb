bfa5f788660fa7585df931f6d3f56b5b2a85c092
Stephen McBride <stevemcbride3@gmail.com>
2020-07-06 06:50:54 -0400

    Updated session routes

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,21 +3,18 @@ class SessionsController < ApplicationController
     end
 
     def create #login action
-        @user = User.find(user_params[:id])
-        if @user.authenticate(user_params[:password])
+        @user = User.find_by(email: params[:email])
+        if @user.authenticate(params[:password])
             session[:user_id] = @user.id
             redirect_to user_path(@user)
         else
             flash[:warning] = "Invalid email or password. Please try again."
-            redirect_to new_session_path
+            redirect_to "/login"
         end
     end
 
     def destroy #logout
         session.destroy
-    end
-
-    def user_params
-        params.require(:user).permit(:email, :password)
+        redirect_to "/login"
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,5 +5,8 @@ Rails.application.routes.draw do
   resources :menus
   resources :restaurants
   resources :users, except: [:index]
+  get "login", to: "sessions#new"
+  post "login", to: "sessions#create"
+  post "logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
