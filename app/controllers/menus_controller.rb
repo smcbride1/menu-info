ce22c09e74c1a3da32112dc5e512501ff71ac4d1
Stephen McBride <stevemcbride3@gmail.com>
2020-07-08 02:10:15 -0400

    Added nested route support to menus controller, created register routes

--- app/controllers/menus_controller.rb
+++ app/controllers/menus_controller.rb
@@ -1,2 +1,14 @@
 class MenusController < ApplicationController
+    def index
+        if params.has_key?(:menu_type)
+            @menus = Menu.where(menu_type_id: params[:menu_type][:id])
+        elsif params.has_key?(:restaurant)
+            @menus = Menu.where(restaurant_id: params[:restaurant][:id])
+        else
+            @menus = Menu.all
+        end
+    end
+
+    def show
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -28,5 +28,9 @@ Rails.application.routes.draw do
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   post "logout", to: "sessions#destroy"
+
+  get "register", to: "users#new"
+  post "register", to: "users#create"
+
   root "application#index"
 end
