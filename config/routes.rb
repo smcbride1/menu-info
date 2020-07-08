d583211eab012efb0250806f8132acedfb4a4089
Stephen McBride <stevemcbride3@gmail.com>
2020-07-08 02:32:28 -0400

    Fixed 'My MENUS' link, created nested routes 'users/menus' and 'users/menu_items'

--- config/routes.rb
+++ config/routes.rb
@@ -1,28 +1,30 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :menu_types do
-    resources :menus
+    resources :menus, only: [:index]
   end
 
   resources :tags do
-    resources :menu_items
+    resources :menu_items, only: [:index]
   end
 
   resources :menu_items
 
   resources :menus
   resources :menus do
-    resources :menu_items
+    resources :menu_items, only: [:index]
   end
 
   resources :restaurants
   resources :restaurants do
-    resources :menus
+    resources :menus, only: [:index]
   end
 
   resources :users, except: [:index]
   resources :users do
-    resources :restaurants
+    resources :restaurants, only: [:index]
+    resources :menus, only: [:index]
+    resources :menu_items, only: [:index]
   end
 
   get "login", to: "sessions#new"
