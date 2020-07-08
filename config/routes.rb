7befba72f0cce47d78c8eb1b3619d726f6bcad38
Stephen McBride <stevemcbride3@gmail.com>
2020-07-07 21:03:25 -0400

    Created nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,32 @@
 Rails.application.routes.draw do
-  resources :menu_types
-  resources :tags
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :menu_types do
+    resources :menus
+  end
+
+  resources :tags do
+    resources :menu_items
+  end
+
   resources :menu_items
+
   resources :menus
+  resources :menus do
+    resources :menu_items
+  end
+
   resources :restaurants
+  resources :restaurants do
+    resources :menus
+  end
+
   resources :users, except: [:index]
+  resources :users do
+    resources :restaurants
+  end
+
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   post "logout", to: "sessions#destroy"
   root "application#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
