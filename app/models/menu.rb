f4840b117bfb4206265b2abc372f6631caedea7b
Stephen McBride <stevemcbride3@gmail.com>
2020-07-10 04:14:48 -0400

    Added menu validations, fixed bug where menus were not showing on the restaurant show page if owner not logged in, changed some visuals

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -3,4 +3,7 @@ class Menu < ApplicationRecord
     has_many :menu_types
     has_many :menu_items, dependent: :destroy
     has_many :tags, through: :menu_items
+
+    validates :name, presence: true, length: { maximum: 100 }
+    validates :description, presence: true, length: { maximum: 1000 }
 end
